3da0d8f311855d52ed09c3eb8cf6ed981fddbae2
algorithm01 <algorithm@mailinator.com>
2017-08-13 18:49:59 -0400

    dsfsdgsdg

--- src/test/java/com/alg/mvnproject/test1.java
+++ src/test/java/com/alg/mvnproject/test1.java
@@ -6,14 +6,53 @@ import org.testng.annotations.Test;
 
 public class test1 {
 	public static WebDriver driver = new FirefoxDriver();
-	
-	
+		
 	@Test
-	public void test() {
+	public void testMethod0() {
 		driver.get("https://www.google.ca");
 		System.out.println("########################");
 		System.out.println("Test pass");
 		driver.quit();
+	}
+	
+	@Test
+	public void testMethod1() {
+		System.out.println("########################");
+		System.out.println("This is test 1");
+		System.out.println("Test pass");
+		
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("########################");
+		System.out.println("This is test 2");
+		System.out.println("Test pass");
+		
+	}
+	
+	@Test
+	public void testMethod3() {
+		System.out.println("########################");
+		System.out.println("This is test 3");
+		System.out.println("Test pass");
+		
+	}
+	
+	@Test
+	public void testMethod4() {
+		System.out.println("########################");
+		System.out.println("This is test 4");
+		System.out.println("Test pass");
+		
+	}
+	
+	@Test
+	public void testMethod5() {
+		System.out.println("########################");
+		System.out.println("This is test 5");
+		System.out.println("Test pass");
+		
 	}
 			
 			
